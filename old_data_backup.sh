07099a6c4685092b167e2feac890e28410745f25
Subhankar Neogi <smnonrocks@users.noreply.github.com>
2017-01-13 16:19:32 +0530

    Update old_data_backup.sh

--- old_data_backup.sh
+++ old_data_backup.sh
@@ -20,20 +20,20 @@
  mkdir /opt/PostgresPlus/9.1AS/Desktop/Old_data_backup/$BACKUP_FOLDER
  #mkdir /opt/PostgresPlus/9.1AS/Desktop/Old_data_backup
  
- pg_dump -a --format=plain --table=v4reg.tbl_entity --inserts --attribute-inserts etrans > /opt/PostgresPlus/9.1AS/Desktop/Old_data_backup/$BACKUP_FOLDER/tbl_entity.sql
- pg_dump -a --format=plain --table=v4map.tbl_node --inserts --attribute-inserts etrans > /opt/PostgresPlus/9.1AS/Desktop/Old_data_backup/$BACKUP_FOLDER/tbl_node.sql 
- pg_dump -a --format=plain --table=v4reg.tbl_entity --inserts --attribute-inserts etrans > /opt/PostgresPlus/9.1AS/Desktop/Old_data_backup/$BACKUP_FOLDER/tbl_entity.sql 
- pg_dump -a --format=plain --table=v4reg.tbl_asset_driver --inserts --attribute-inserts etrans > /opt/PostgresPlus/9.1AS/Desktop/Old_data_backup/$BACKUP_FOLDER/tbl_asset_driver.sql 
- pg_dump -a --format=plain --table=v4reg.tbl_user --inserts --attribute-inserts etrans > /opt/PostgresPlus/9.1AS/Desktop/Old_data_backup/$BACKUP_FOLDER/tbl_user.sql 
- pg_dump -a --format=plain --table=v4ugm.tbl_role --inserts --attribute-inserts etrans > /opt/PostgresPlus/9.1AS/Desktop/Old_data_backup/$BACKUP_FOLDER/tbl_role.sql 
- pg_dump -a --format=plain --table=v4ugm.tbl_module --inserts --attribute-inserts etrans > /opt/PostgresPlus/9.1AS/Desktop/Old_data_backup/$BACKUP_FOLDER/tbl_module.sql 
- pg_dump -a --format=plain --table=v4ugm.tbl_service --inserts --attribute-inserts etrans > /opt/PostgresPlus/9.1AS/Desktop/Old_data_backup/$BACKUP_FOLDER/tbl_service.sql 
- pg_dump -a --format=plain --table=v4ugm.tbl_role_service --inserts --attribute-inserts etrans > /opt/PostgresPlus/9.1AS/Desktop/Old_data_backup/$BACKUP_FOLDER/tbl_role_service.sql 
- pg_dump -a --format=plain --table=v4trip.tbl_maintain --inserts --attribute-inserts etrans > /opt/PostgresPlus/9.1AS/Desktop/Old_data_backup/$BACKUP_FOLDER/tbl_maintain.sql 
- pg_dump -a --format=plain --table=v4stock.tbl_lab_stock --inserts --attribute-inserts etrans > /opt/PostgresPlus/9.1AS/Desktop/Old_data_backup/$BACKUP_FOLDER/tbl_lab_stock.sql
- pg_dump -a --format=plain --table=v4stock.tbl_lab_stock --inserts --attribute-inserts etrans > /opt/PostgresPlus/9.1AS/Desktop/Old_data_backup/$BACKUP_FOLDER/tbl_amc_activity.sql
-  pg_dump -a --format=plain --table=v4ugm.tbl_user_role --inserts --attribute-inserts etrans > /opt/PostgresPlus/9.1AS/Desktop/Old_data_backup/$BACKUP_FOLDER/tbl_user_role.sql
-  pg_dump -a --format=plain --table=schema_v3.tbl_mst_entity_user_map --inserts --attribute-inserts etrans > /opt/PostgresPlus/9.1AS/Desktop/Old_data_backup/$BACKUP_FOLDER/tbl_mst_entity_user_map.sql
+ pg_dump -a --format=plain --table=v4reg.tbl_entity --inserts --attribute-inserts dbname > /opt/PostgresPlus/9.1AS/Desktop/Old_data_backup/$BACKUP_FOLDER/tbl_entity.sql
+ pg_dump -a --format=plain --table=v4map.tbl_node --inserts --attribute-inserts dbname > /opt/PostgresPlus/9.1AS/Desktop/Old_data_backup/$BACKUP_FOLDER/tbl_node.sql 
+ pg_dump -a --format=plain --table=v4reg.tbl_entity --inserts --attribute-inserts dbname > /opt/PostgresPlus/9.1AS/Desktop/Old_data_backup/$BACKUP_FOLDER/tbl_entity.sql 
+ pg_dump -a --format=plain --table=v4reg.tbl_asset_driver --inserts --attribute-inserts dbname > /opt/PostgresPlus/9.1AS/Desktop/Old_data_backup/$BACKUP_FOLDER/tbl_asset_driver.sql 
+ pg_dump -a --format=plain --table=v4reg.tbl_user --inserts --attribute-inserts dbname > /opt/PostgresPlus/9.1AS/Desktop/Old_data_backup/$BACKUP_FOLDER/tbl_user.sql 
+ pg_dump -a --format=plain --table=v4ugm.tbl_role --inserts --attribute-inserts dbname > /opt/PostgresPlus/9.1AS/Desktop/Old_data_backup/$BACKUP_FOLDER/tbl_role.sql 
+ pg_dump -a --format=plain --table=v4ugm.tbl_module --inserts --attribute-inserts dbname > /opt/PostgresPlus/9.1AS/Desktop/Old_data_backup/$BACKUP_FOLDER/tbl_module.sql 
+ pg_dump -a --format=plain --table=v4ugm.tbl_service --inserts --attribute-inserts dbname > /opt/PostgresPlus/9.1AS/Desktop/Old_data_backup/$BACKUP_FOLDER/tbl_service.sql 
+ pg_dump -a --format=plain --table=v4ugm.tbl_role_service --inserts --attribute-inserts dbname > /opt/PostgresPlus/9.1AS/Desktop/Old_data_backup/$BACKUP_FOLDER/tbl_role_service.sql 
+ pg_dump -a --format=plain --table=v4trip.tbl_maintain --inserts --attribute-inserts dbname > /opt/PostgresPlus/9.1AS/Desktop/Old_data_backup/$BACKUP_FOLDER/tbl_maintain.sql 
+ pg_dump -a --format=plain --table=v4stock.tbl_lab_stock --inserts --attribute-inserts dbname > /opt/PostgresPlus/9.1AS/Desktop/Old_data_backup/$BACKUP_FOLDER/tbl_lab_stock.sql
+ pg_dump -a --format=plain --table=v4stock.tbl_lab_stock --inserts --attribute-inserts dbname > /opt/PostgresPlus/9.1AS/Desktop/Old_data_backup/$BACKUP_FOLDER/tbl_amc_activity.sql
+  pg_dump -a --format=plain --table=v4ugm.tbl_user_role --inserts --attribute-inserts dbname > /opt/PostgresPlus/9.1AS/Desktop/Old_data_backup/$BACKUP_FOLDER/tbl_user_role.sql
+  pg_dump -a --format=plain --table=schema_v3.tbl_mst_entity_user_map --inserts --attribute-inserts dbname > /opt/PostgresPlus/9.1AS/Desktop/Old_data_backup/$BACKUP_FOLDER/tbl_mst_entity_user_map.sql
   
   echo "TACKING OLDA DATA BACKUP HAS COMPLEATED TO CHECK IT PLESE FIND /opt/PostgresPlus/9.1AS/Desktop/Old_data_backup/$BACKUP_FOLDER derectery for detail"
  exit
